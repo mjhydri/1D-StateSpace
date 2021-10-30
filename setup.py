800f81410c81ac37b8699953274482c92fe1af5c
mjhydri <mj.hydri@gmail.com>
2021-10-29 22:12:53 -0400

    binaries update

--- setup.py
+++ setup.py
@@ -17,13 +17,15 @@ import distutils.cmd
 
 # Required packages
 REQUIRED_PACKAGES = [
-    'numpy==1.19.2',
+    'numpy',
     'Cython',
     'librosa>=0.8.0',
-    'numba==0.51.2', 
+    'numba==0.54.1', # Manually specified here as librosa incorrectly states that it is compatible with the latest version of numba although 0.50.0 is not compatible. 
     'scipy',
     'mido>=1.2.6',
     'pytest',
+    #'pyaudio',
+    ##'pyfftw',
     'torch',
     'Matplotlib',
     'BeatNet>=0.0.2',
