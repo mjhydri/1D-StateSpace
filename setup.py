b29bb865c7f370c8d6b82632dec9325b80d6fb7b
mjhydri <mj.hydri@gmail.com>
2021-10-29 21:19:15 -0400

    binaries are updated

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setuptools.setup(
     packages=find_packages(where="src"),
     # packages=find_packages(),
     include_package_data=True,
-    install_requires=REQUIRED_PACKAGES,requirements
+    install_requires=REQUIRED_PACKAGES,
 
     # Metadata to display on PyPI
     author="Mojtaba Heydari",
